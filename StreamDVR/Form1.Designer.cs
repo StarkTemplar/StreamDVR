dd856191ad5557ae9a520582ab6bbf4b98e0c9a2
StarkTemplar <gilbert.42989@gmail.com>
2016-01-06 00:03:00 -0500

    1st commit. Empty form

---
+++ StreamDVR/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace StreamDVR
+{
+    partial class rootForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // rootForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1321, 662);
+            this.Name = "rootForm";
+            this.Text = "StreamDVR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
